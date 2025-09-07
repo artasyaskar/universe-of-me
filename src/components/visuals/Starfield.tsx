afe8094fd3c125516fadfe2ac56e24ef3196686b
Artas Yaskar <artasyaskar@gmail.com>
2025-09-07 23:08:10 +0500

    Create Starfield.tsx

---
+++ src/components/visuals/Starfield.tsx
@@ -0,0 +1,31 @@
+import { useState, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import * as THREE from 'three';
+import * as random from 'maath/random/dist/maath-random.esm';
+
+export default function Starfield(props: any) {
+  const ref = useRef<THREE.Points>(null);
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }));
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial
+          transparent
+          color="#ffffff"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
